7442ddf501397649fd23d12af385abdcc91a02a1
Abhranil Sen <abhranilsen98@gmail.com>
2025-08-25 13:44:26 +0530

    Update index.html to hide keys, update service worker, and PushTestApp

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCHh0BlMhOIWeNrErttmxK1D2vR4IkXgBU",
+  apiKey: "your api key",
   authDomain: "demoproject-2f4cb.firebaseapp.com",
   projectId: "demoproject-2f4cb",
   storageBucket: "demoproject-2f4cb.appspot.com",  
